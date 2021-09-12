ef9d03ae94f39d063c80a7219d9b548758b81fa3
marcal-morais <marcal.2@gmail.com>
2021-09-12 17:56:13 -0300

    Configurações finalizadas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config({ path: __dirname + '/.env' });
+import dotenv from 'dotenv';
 
-import express from "express";
-import helmet from "helmet";
-import cors from "cors";
+import express, { ErrorRequestHandler } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import routes from './routes';
+
+dotenv.config({ path: `${__dirname}/.env` });
 
 const app = express();
 
@@ -11,10 +14,25 @@ app.use(helmet());
 app.use(
   cors({
     origin: [`${process.env.CORS}`],
-  })
-)
+  }),
+);
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.set('X-Powered-By', 'PHP/7.1.7');
+  next();
+});
+
+app.use(routes);
+
+const errorHandler: ErrorRequestHandler = (err, req, res) => {
+  if (process.env.NODE_ENV === 'development') {
+    return res.status(500).json(err);
+  }
+  return res.status(500).json({ error: 'Internal server error' });
+};
+
+app.use(errorHandler);
 
 export default app;
